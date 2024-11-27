9e802fb8f6a280f6487ec7a497010f79432d26e8
Romain CHENARD <rom@Romains-MacBook-Air.local>
2024-11-27 21:09:05 +0100

    parallel + contextTimeOut

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"context"
 	"design-patterns/math"
 	"sync"
+	"time"
 )
 
-func generator(min int64, max int64, wg *sync.WaitGroup) (chan int64, chan int) {
+func generator(min int64, max int64, wg *sync.WaitGroup, ctx context.Context) (chan int64, chan int) {
+
 	out := make(chan int64)
 	done := make(chan int)
 	go func() {
 		for i := min; i < max; i++ {
-			out <- i
+			select {
+			case <-ctx.Done():
+				break
+			default:
+				out <- i
+			}
 		}
 		done <- 1
 		wg.Done()
@@ -60,14 +68,17 @@ func receiver(in []chan int64, done chan int) {
 func parallel(start int64, end int64) {
 	var wg sync.WaitGroup
 	wg.Add(1)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	defer cancel()
 
-	gen, done := generator(1_000_000, 1_500_000, &wg)
+	gen, done := generator(1_000_000, 1_500_000, &wg, ctx)
 
 	chans := splitter(15, gen, analyser)
 
 	receiver(chans, done)
 
 	wg.Wait()
+
 }
 
 func serie(start int64, end int64) {
