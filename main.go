28aad41b0688699dcfe6eab8bbab37020720a217
Romain CHENARD <rom@Romains-MacBook-Air.local>
2024-11-27 21:02:51 +0100

    parallel

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"design-patterns/math"
 	"sync"
 )
 
@@ -18,25 +18,67 @@ func generator(min int64, max int64, wg *sync.WaitGroup) (chan int64, chan int) 
 	return out, done
 }
 
-func receiver(in chan int64, done chan int) {
+func splitter(chanNumber int, in chan int64, gof func(chan int64, chan int64)) []chan int64 {
+	out := []chan int64{}
+	for i := 0; i < chanNumber; i++ {
+		chn := make(chan int64)
+		out = append(out, chn)
+		gof(in, chn)
+	}
+	return out
+}
+
+func analyser(in chan int64, out chan int64) {
 	go func() {
 		for {
 			select {
-			case <-done:
-				return
 			case num := <-in:
-				fmt.Println("--->", num)
+				if math.IsPrime(num) {
+					out <- num
+				}
 			}
 		}
 	}()
 }
 
-func main() {
+func receiver(in []chan int64, done chan int) {
+	for _, ch := range in {
+		go func() {
+			for {
+				select {
+				case <-done:
+					return
+				case num := <-ch:
+					num++
+					// fmt.Println("--->", num)
+				}
+			}
+		}()
+	}
+}
+
+func parallel(start int64, end int64) {
 	var wg sync.WaitGroup
 	wg.Add(1)
 
-	gen, don := generator(1, 1_000_000, &wg)
-	receiver(gen, don)
+	gen, done := generator(1_000_000, 1_500_000, &wg)
+
+	chans := splitter(15, gen, analyser)
+
+	receiver(chans, done)
 
 	wg.Wait()
+}
+
+func serie(start int64, end int64) {
+	var i int64
+	for i = start; i < end; i++ {
+		if math.IsPrime(i) {
+			// fmt.Println("--->", i)
+		}
+	}
+}
+
+func main() {
+
 }
