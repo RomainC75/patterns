288c2d5ca323c79b86493bf3c5e78afca70c3c82
Romain CHENARD <rom@Romains-MacBook-Air.local>
2024-11-29 22:19:26 +0100

    feat: 🎸 parallel+ Generator+accumulator

--- finder/common.go
+++ finder/common.go
@@ -4,9 +4,12 @@ import (
 	"design-patterns/strategies"
 	"design-patterns/types"
 	"errors"
+	"fmt"
+	"sync"
 )
 
 type SFinder[T types.CustomInt] struct {
+	res      []T
 	Strategy IFinderSrategy[T]
 }
 
@@ -41,11 +44,69 @@ func (s *SFinder[T]) RunSerie(min T, max T) []T {
 }
 
 func (s *SFinder[T]) RunParallel(min T, max T) []T {
-	res := []T{}
-	for i := min; i < max; i++ {
-		if s.Strategy.IsOk(i) {
-			res = append(res, i)
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	numbers, done := s.Generator(min, max)
+	res := s.Parallel(8, numbers)
+
+	s.Accumulator(1, res, done)
+
+	wg.Wait()
+
+	return []T{}
+}
+
+func (s *SFinder[T]) Generator(min T, max T) (chan T, chan int) {
+	gen := make(chan T)
+	done := make(chan int)
+	go func() {
+		for i := min; i <= max; i++ {
+			gen <- i
 		}
+	}()
+	return gen, done
+}
+
+func (s *SFinder[T]) Parallel(chanNum int, inputChan chan T) chan T {
+
+	// ! capacity ?
+
+	// out := make([]chan T, chanNum)
+	res := make(chan T)
+
+	// for i := 0; i < chanNum; i++ {
+	// 	ch := make(chan T)
+	// 	out = append(out, ch)
+	// }
+
+	for i := 0; i < chanNum; i++ {
+		go func() {
+			for {
+				j := <-inputChan
+				if s.Strategy.IsOk(j) {
+					res <- j
+					fmt.Println(i, " -> ", j)
+				}
+			}
+		}()
 	}
 	return res
+}
+
+func (s *SFinder[T]) Accumulator(chanNum int, inputChan chan T, done chan int) {
+	// res := []T{}
+	go func() {
+		// resCh := make(chan []T)
+
+		for {
+			select {
+			case <-done:
+				return
+			case val := <-inputChan:
+				fmt.Println("- ", val)
+			}
+			// res = append(res, val)
+		}
+	}()
 }
--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"design-patterns/finder"
+	"fmt"
+	"log"
+)
+
+var min int64 = 1_000_000
+var max int64 = 10_000_000
+
 func main() {
+	f, err := finder.NewFinder[int64](finder.Prime)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	res := f.RunParallel(min, max)
+	fmt.Println("res : ", res)
 
 }
