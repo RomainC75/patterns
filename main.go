bd7a119c51e4f1e5afe472677fd4f15f276e9637
Romain CHENARD <rom@Romains-MacBook-Air.local>
2024-12-03 22:02:52 +0100

    feat: 🎸 tests pointer to pointer to a slice

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import "fmt"
 
+func intFn() {
+	var v int = 100
+
+	var pt *int = &v
+
+	var ppt **int = &pt
+
+	fmt.Println("v", v)
+	fmt.Println("pt", pt, *pt)
+	fmt.Println("ppt", ppt, **ppt)
+
+	v = 200
+	fmt.Println("->", **ppt)
+	fmt.Println("->", **ppt)
+}
+
+func arrayFn() {
+	arr := []int{1, 2, 3}
+	arrPt := &arr
+	arrPpt := &arrPt
+
+	fmt.Println("-> arr", arr)
+	fmt.Println("-> arrPt", *arrPt)
+	fmt.Println("-> arrPpt", **arrPpt)
+
+	*arrPpt = &[]int{3, 4, 5}
+	fmt.Println("---> arrPpt", **arrPpt)
+
+}
+
+func main() {
+	arrayFn()
 }
--- main_test.go
+++ main_test.go
@@ -1,14 +1,11 @@
 package main
 
-import "testing"
+// import "testing"
 
-var start int64 = 1_000_000
-var end int64 = 2_000_000
+// func BenchmarkParallel(b *testing.B) {
+// 	parallel(start, end)
+// }
 
-func BenchmarkParallel(b *testing.B) {
-	parallel(start, end)
-}
-
-func BenchmarkSerie(b *testing.B) {
-	serie(start, end)
-}
+// func BenchmarkSerie(b *testing.B) {
+// 	serie(start, end)
+// }
